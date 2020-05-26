fa84582e8a9006ffc869bbb8cf541b13bda6668a
Monjur <gm3.bappy@gmail.com>
2020-05-27 01:55:27 +0600

    Update setup.sh
    Custom Extensions, i.e: NetCDF Added

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -54,9 +54,14 @@ create_dir ${resources_dir}/plugins
 pushd ${resources_dir}/plugins
 #Extensions
 
-array=(geoserver-$GS_VERSION-vectortiles-plugin.zip geoserver-$GS_VERSION-wps-plugin.zip geoserver-$GS_VERSION-printing-plugin.zip \
+array=(geoserver-$GS_VERSION-vectortiles-plugin.zip geoserver-$GS_VERSION-css-plugin.zip \
+geoserver-$GS_VERSION-csw-plugin.zip geoserver-$GS_VERSION-wps-plugin.zip geoserver-$GS_VERSION-printing-plugin.zip \
 geoserver-$GS_VERSION-libjpeg-turbo-plugin.zip geoserver-$GS_VERSION-control-flow-plugin.zip \
-geoserver-$GS_VERSION-pyramid-plugin.zip geoserver-$GS_VERSION-gdal-plugin.zip geoserver-$GS_VERSION-monitor-plugin.zip )
+geoserver-$GS_VERSION-pyramid-plugin.zip geoserver-$GS_VERSION-gdal-plugin.zip \
+geoserver-$GS_VERSION-sldservice-plugin.zip geoserver-$GS_VERSION-monitor-plugin.zip \
+geoserver-$GS_VERSION-netcdf-plugin.zip geoserver-$GS_VERSION-netcdf-out-plugin.zip \
+geoserver-$GS_VERSION-importer-plugin.zip geoserver-$GS_VERSION-charts-plugin.zip)
+
 for i in "${array[@]}"
 do
     url="https://sourceforge.net/projects/geoserver/files/GeoServer/${GS_VERSION}/extensions/${i}/download"
